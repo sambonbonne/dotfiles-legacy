6b68e85b8084aab9c986e05c635766dac0925131
Samuel Denis <samuel@denis.tech>
2015-10-02 21:25:04 +0200

    Add install script, that's a good idea I think

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo "Hi! What a good day to prepare a computer for more productivity. Let's go!"
+
+# Clone dotfiles
+read -p "Do you need to clone dotfiles in ~/dev/dotfiles ?" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  type git >/dev/null 2>&1 || { echo "Git doesn't exists, but what is this shitty computer ?!"; exit 1; }
+  mkdir -p ~/dev
+  git clone https://github.com/smumu/dotfiles ~/dev/dotfiles
+fi
+
+# Put ZSH config
+echo "ZSH"
+ln -s ~/dev/dotfiles/profile ~/.profile
+ln -s ~/dev/dotfiles/zshrc ~/.zshr
+ln -s ~/dev/dotfiles/zsh ~/.zsh
+type zsh >/dev/null 2>&1 && chsh -s /bin/zsh || echo "ZSH is configured but not installed, that's problematic ..."
+
+# And Vim config
+echo "Vim"
+ln -s ~/dev/dotfiles/vimrc ~/.vimrc
+mkdir -p ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+type vim >/dev/null 2>&1 && vim +PluginInstall +qall || echo "Vim is configured but not installed, will you survive ?"
+
+# Don't forget Tmux
+echo "Tmux"
+ln -s ~/dev/dotfiles/tmux.conf ~/.tmux.conf
+
+echo "My work here is done, have a nice day!"
