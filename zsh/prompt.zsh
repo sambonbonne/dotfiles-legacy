9b8dd0988b8b4ab82fd0cfb4053cb8695782fd26
Samuel DENIS <sdenis@catalisio.com>
2016-04-13 10:41:37 +0200

    ZSH: prompt refactoring

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,32 +2,48 @@
 autoload -U promptinit && promptinit
 setopt PROMPT_SUBST
 
-local virtualenv_prompt_parse='$([[ "$VIRTUAL_ENV" != "" ]] && echo " $(basename $VIRTUAL_ENV)")'
+local virtualenv_prompt_parse='$([[ "${VIRTUAL_ENV}" != "" ]] && echo " $(basename ${VIRTUAL_ENV})")'
 
-local nbsp=$'\u00A0'
-local default_username='samuel'
+PROMPT=""
 
-if [[ -n "$SSH_CLIENT" || "$LOGNAME" != "$default_username" ]]; then
-    PROMPT="%{$fg_no_bold[cyan]%}%n%{$reset_color%}@%{$fg_no_bold[blue]%}%m%{$reset_color%} %{$fg_no_bold[yellow]%}%~%{$fg_no_bold[red]%}${virtualenv_prompt_parse}%{$reset_color%}"
-else
-    #PROMPT="%{$fg_no_bold[blue]%}%~ %(?.%{$fg_no_bold[green]%}→.%{$fg_bold[red]%}!)%{$reset_color%}$nbsp"
-    PROMPT="%{$fg_no_bold[blue]%}%~%{$fg_no_bold[red]%}${virtualenv_prompt_parse}%{$reset_color%}"
+local _default_username="samuel"
+if [[ "${LOGNAME}" != "${USER}" || "${LOGNAME}" != "${_default_username}" || "${USER}" != "${_default_username}" ]] ; then
+	PROMPT="${PROMPT}%{$fg_no_bold[cyan]%}%n%{$reset_color%}"
 fi
-BASE_PROMPT="$PROMPT"
+unset _default_username
 
+if [[ -n "${SSH_CLIENT}" ]] ; then
+	[ -n "${PROMPT}" ] && PROMPT="${PROMPT}@"
+	PROMPT="${PROMPT}%{$fg_no_bold[blue]%}%M%{$reset_color%}"
+fi
+
+# no SSH nor different username : take a really light prompt
+[ -z "${PROMPT}" ] && PROMPT="%{$fg_no_bold[blue]%}%~"
+
+PROMPT="${PROMPT} %{$fg_no_bold[yellow]%}%~%{$fg_no_bold[red]%}${virtualenv_prompt_parse}%{$reset_color%}"
+BASE_PROMPT="${PROMPT}"
+
+# right prompt
 source ~/.zsh/git
 local git_prompt_parse='$(git_prompt_string)'
 RPROMPT="[%{$fg_no_bold[white]%}%T%{$reset_color%} %(?.%{$fg_no_bold[green]%}.%{$fg_no_bold[red]%})%?%{$reset_color%}]%(1j. (%{$fg_no_bold[magenta]%}%j%{$reset_color%}J%).)${git_prompt_parse}"
 
+# current vi mode and last status
 function zle-line-finish zle-keymap-select {
+    PROMPT="${BASE_PROMPT} "
+
+	local _nbsp=$'\u00A0'
     case "${KEYMAP}" in
         vicmd)
-            PROMPT="$BASE_PROMPT %{$fg_no_bold[yellow]%}?%{$reset_color%} "
+            PROMPT="${PROMPT}%{$fg_no_bold[yellow]%}?"
             ;;
         *)
-	    	PROMPT="$BASE_PROMPT %(?.%{$fg_no_bold[green]%}→.%{$fg_bold[red]%}!)%{$reset_color%}$nbsp"
+	        PROMPT="${PROMPT}%(?.%{$fg_no_bold[green]%}→.%{$fg_bold[red]%}!)"
             ;;
     esac
+
+	PROMPT="${PROMPT}%{$reset_color%}${_nbsp}"
+
 	zle && zle reset-prompt
 }
 zle -N zle-line-finish
