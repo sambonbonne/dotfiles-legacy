11816cf8cd231aea25530e47363c433252d8ad33
Samuel DENIS <sdenis@catalisio.com>
2016-04-13 10:42:16 +0200

    ZSH: reorder plugins and add substring search

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -13,7 +13,17 @@ installPluginManager() {
 
 zplug "b4b4r07/zplug"
 
+zplug "zsh-users/zsh-completions"
+
 zplug "zsh-users/zsh-autosuggestions"
+
+zplug "zsh-users/zsh-history-substring-search", nice:11
+# in normal mode, up/down keys
+bindkey "^[[A" history-substring-search-up
+bindkey "^[[B" history-substring-search-down
+# in vi mode, j/k keys
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
 zplug "zsh-users/zsh-syntax-highlighting", nice:10
@@ -21,7 +31,9 @@ zplug "zsh-users/zsh-syntax-highlighting", nice:10
 zplug "hlissner/zsh-autopair"
 
 zplug "chrissicool/zsh-256color"
+
 zplug "sharat87/zsh-vim-mode"
+
 zplug "jreese/zsh-titles", if:"which tmux"
 
 zplug "marzocchi/zsh-notify", if:"which notify-send"
@@ -29,7 +41,6 @@ zplug "marzocchi/zsh-notify", if:"which notify-send"
 zplug "Tarrasch/zsh-bd"
 
 zplug "akoenig/gulp.plugin.zsh", if:"which gulp"
-zplug "zsh-users/zsh-completions"
 
 #zplug "nvbn/thefuck", as:"command", if:"which python && which pip", do:"pip install --user --upgrade -r requirements.txt"
 
