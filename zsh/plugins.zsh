c8945c11f050c791f626ad404bbdc817a9dee0c2
Samuel DENIS <sdenis@catalisio.com>
2016-04-25 15:01:53 +0200

    ZSH: plugin load order, new plugins

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -17,7 +17,7 @@ zplug "zsh-users/zsh-completions"
 
 zplug "zsh-users/zsh-autosuggestions"
 
-zplug "zsh-users/zsh-history-substring-search", nice:11
+zplug "zsh-users/zsh-history-substring-search", nice:12
 # in normal mode, up/down keys
 bindkey "^[[A" history-substring-search-up
 bindkey "^[[B" history-substring-search-down
@@ -26,13 +26,16 @@ bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
-zplug "zsh-users/zsh-syntax-highlighting", nice:10
+zplug "zsh-users/zsh-syntax-highlighting", nice:11
 
-zplug "hlissner/zsh-autopair"
+zplug "hlissner/zsh-autopair", nice:10
 
 zplug "chrissicool/zsh-256color"
 
 zplug "sharat87/zsh-vim-mode"
+
+zplug "hchbaw/zce.zsh"
+bindkey "^F" zce
 
 zplug "jreese/zsh-titles", if:"which tmux"
 
@@ -41,6 +44,8 @@ zplug "marzocchi/zsh-notify", if:"which notify-send"
 zplug "Tarrasch/zsh-bd"
 
 zplug "akoenig/gulp.plugin.zsh", if:"which gulp"
+
+zplug "Seinh/git-prune", if:"which git"
 
 #zplug "nvbn/thefuck", as:"command", if:"which python && which pip", do:"pip install --user --upgrade -r requirements.txt"
 
