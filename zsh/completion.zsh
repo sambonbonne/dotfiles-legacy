b2171a9b0b4ef53e79cc4a161b1c0bab0f17023b
Samuel DENIS <sdenis@catalisio.com>
2016-06-17 18:29:17 +0200

    ZSH: better completion config

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -7,10 +7,18 @@ zstyle ':completion:*' menu select=2
 zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin
 
 # don't complete the same thing twice
-zstyle ':completion:*:*:(ls|rm|mv|cp|vi|vim|nvim):*' ignore-line true
+zstyle ':completion:*:*:(ls|rm|mv|cp|vi|vim|nvim|view|nview|vimdiff|nvimdiff):*' ignore-line true
 
-# for file editing, ignore tmp
-zstyle ':completion:*:*:(vi|vim|nvim):*:*files' ignored-patterns '.?*(~|.swp)'
+# for editing completion ignore tmp files
+zstyle ':completion:*:*:(vi|vim|nvim|view|nview):*:*files' ignored-patterns '*~|*.swp'
+
+# some pattern for scripts
+zstyle ':completion:*:*:sh:*:*files' file-patterns '*.(sh)'
+zstyle ':completion:*:*:bash:*:*files' file-patterns '*.(sh|bash)'
+zstyle ':completion:*:*:zsh:*:*files' file-patterns '*.(sh|zsh)'
+zstyle ':completion:*:*:(node|nodejs):*:*files' file-patterns '*.(js|mjs)'
+zstyle ':completion:*:*:php:*:*files' file-patterns '*.php'
+zstyle ':completion:*:*:(python|python2|python3|py):*:*files' file-patterns '*.py'
 
 # explicitly say nothing found
 zstyle ':completion:*:warnings' format 'No matches for: %d'
