0e66c69bf5b05ad6c9cbaa27210357f6a5605062
Samuel DENIS <samuel@denis.tech>
2016-12-23 18:06:06 +0100

    Profile: add vless function (autodetect editor)

--- config/profile/tools.sh
+++ config/profile/tools.sh
@@ -24,3 +24,24 @@ _default_editor() {
 }
 export EDITOR="$(_default_editor)"
 export GIT_EDITOR="${EDITOR}"
+
+# a simple Vim pager
+vless() {
+  local pager_command=''
+
+  if command -v nvim >/dev/null 2>&1; then
+    pager_command="nvim"
+  else
+    pager_command="vim"
+  fi
+
+  pager_command="${pager_command} -R -c 'nnoremap q :q!<Enter>' -c 'set noswapfile'"
+
+  if [[ "$#" -ne 1 ]]; then
+    pager_command="${pager_command} -"
+  else
+    pager_command="${pager_command} ${1}"
+  fi
+
+  eval "${pager_command}"
+}
