b7a9bd48152cec3d9a11c75ab0bd3c9d5aa9c1fb
Samuel DENIS <samuel@denis.tech>
2016-12-26 22:17:13 +0100

    Install: add TPM installation

--- install/softwares/tmux.sh
+++ install/softwares/tmux.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+mkdir -p ~/.tmux/plugins
 link_config "tmux.conf"
+command -v git >/dev/null 2>&1 && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || echo "Can't install Tmux plugins manager"
 check_command "tmux"
