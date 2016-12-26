3409b9b22b592c453d6603e233d8523305806414
Samuel DENIS <samuel@denis.tech>
2016-12-26 22:20:01 +0100

    Tmux: forgot to link config during installation

--- install/softwares/tmux.sh
+++ install/softwares/tmux.sh
@@ -2,5 +2,6 @@
 
 mkdir -p ~/.tmux/plugins
 link_config "tmux.conf"
+link_config "tmux"
 command -v git >/dev/null 2>&1 && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || echo "Can't install Tmux plugins manager"
 check_command "tmux"
