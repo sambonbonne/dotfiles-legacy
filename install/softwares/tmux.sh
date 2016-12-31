02d39551ceff557be5364a4ce8b8c9f7b528d8ec
Samuel DENIS <samuel@denis.tech>
2016-12-31 13:42:52 +0100

    Fix all soft installation and use new mkdir function

--- install/softwares/tmux.sh
+++ install/softwares/tmux.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 
-mkdir -p ~/.tmux/plugins
 link_config "tmux.conf"
 link_config "tmux"
-command -v git >/dev/null 2>&1 && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || echo "Can't install Tmux plugins manager"
+
+ensure_directory .tmux/plugins
+
+_tmux_tpm_dir="${INSTALL_DIR}/.tmux/plugins/tpm"
+
+if [ ! -d "${_tmux_tpm_dir}" ]; then
+  command -v git >/dev/null 2>&1 && git clone https://github.com/tmux-plugins/tpm "${_tmux_tpm_dir}" || echo "Can't install Tmux plugins manager"
+fi
+
 check_command "tmux"
