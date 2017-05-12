5b7801aad0261ade09f18672b96a221b09e16128
Samuel DENIS <samuel@denis.tech>
2017-05-12 21:01:48 +0200

    Prepare difftool configuration

--- config/profile/tools.sh
+++ config/profile/tools.sh
@@ -19,11 +19,10 @@ _default_editor() {
   [ -n "${vim_path}" ] && echo "${vim_path}" && return 0
 
   echo "vi" && return 0
-
-  return 1
 }
 export EDITOR="$(_default_editor)"
 export GIT_EDITOR="${EDITOR}"
+[ "${EDITOR}" = "nvim" ] && alias vim='nvim'
 
 # a simple Vim pager
 vless() {
@@ -45,3 +44,19 @@ vless() {
 
   eval "${pager_command}"
 }
+
+# a more compatible difftool
+_default_difftool() {
+  local nvim_path="$(command -v nvim)"
+  [ -n "${nvim_path}" ] && echo "${nvim_path} -d" && return 0
+
+  local vim_path="$(command -v vim)"
+  [ -n "${vim_path}" ] && echo "${vim_path} -d" && return 0
+
+  local diff_so_fancy_path="$(command -v diff-so-fancy)"
+  [ -n "${diff_so_fancy_path}" ] && echo "${diff_so_fancy_path}" && return 0
+
+  echo "" && return 0
+}
+export DIFFTOOL="$(_default_difftool)"
+export GIT_DIFFTOOL="${_default_difftool}"
