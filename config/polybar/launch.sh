79a35f2683504bd6a349ba7b2162dd8d218f197c
Samuel DENIS <samuel@denis.tech>
2019-08-12 13:08:36 +0200

    Make Polybar use main screen

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u "$(id -u)" -x polybar >/dev/null; do sleep 1; done
 
-polybar top &
+MONITOR="$(xrandr --query | grep primary | cut -d ' ' -f 1)" polybar top &
