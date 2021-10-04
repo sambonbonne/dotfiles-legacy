019e56fe6f5c05692d058a7fb0bb2ba48d1b02ab
sambonbonne <sam@wekk.io>
2021-10-04 09:07:07 +0200

    [WIP] ZSH: replace zplug with zinit, add zsh-command-time plugin

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,31 +1,28 @@
-ZPLUG_HOME="${HOME}/.zsh/zplug"
-ZPLUG_MAIN="${ZPLUG_HOME}/init.zsh"
-ZPLUG_CACHE_DIR="${ZPLUG_HOME}/cache"
+ZINIT_HOME="${HOME}/.cache/.zinit/bin"
+ZINIT_MAIN="${ZINIT_HOME}/zinit.zsh"
 
-installPluginManager() {
-	mkdir -p "${ZPLUG_HOME}"
-	git clone https://github.com/zplug/zplug "${ZPLUG_HOME}"
-	source "${ZPLUG_MAIN}"
-}
-
-if [[ -f "${ZPLUG_MAIN}" ]]; then
-  source "${ZPLUG_MAIN}"
-else
-  installPluginManager
+if [ ! -f "${ZINIT_MAIN}" ]; then
+  mkdir -p "${ZINIT_HOME}"
+  git clone https://github.com/zdharma/zinit "${ZINIT_HOME}"
 fi
 
-zstyle :zplug:tag depth 10
-zstyle ":zplug:config:setopt" only_subshell extended_glob
+source "${ZINIT_MAIN}"
 
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
 
-zplug "zplug/zplug"
+zinit ice src"src"
+zinit load "zsh-users/zsh-completions"
 
-zplug "zsh-users/zsh-completions"
-
-zplug "zsh-users/zsh-autosuggestions"
+zinit load "zsh-users/zsh-autosuggestions"
 bindkey '^[ ' autosuggest-accept
 
-zplug "zsh-users/zsh-history-substring-search", defer:3
+zinit load "hlissner/zsh-autopair"
+
+export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+zinit load "zsh-users/zsh-syntax-highlighting"
+
+zinit load "zsh-users/zsh-history-substring-search"
 # in normal mode, up/down keys
 bindkey "^[[A" history-substring-search-up
 bindkey "^[[B" history-substring-search-down
@@ -33,37 +30,30 @@ bindkey "^[[B" history-substring-search-down
 bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
 
-export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zinit load "chrissicool/zsh-256color"
 
-zplug "hlissner/zsh-autopair", defer:2
+zinit ice has"tmux"
+zinit load "jreese/zsh-titles"
 
-zplug "chrissicool/zsh-256color"
+# # zplug "MichaelAquilina/zsh-auto-notify", if:"{ command -v notify-send ; } >/dev/null 2>&1"
+# # export AUTO_NOTIFY_THRESHOLD=20
+# # export AUTO_NOTIFY_TITLE="· %command → %exit_code ·"
+# # export AUTO_NOTIFY_BODY="Duration: %elapsed seconds"
+# zplug "marzocchi/zsh-notify", if:"{ command -v notify-send ; } >/dev/null 2>&1"
+# #zstyle ':notify:*' blacklist-regex 'kak|nvim|vim'
+# zstyle ':notify:*' enable-on-ssh yes
+# zstyle ':notify:*' command-complete-timeout 20
+# zstyle ':notify:*' error-title "(╯°□°)╯ ︵ ┻━┻"
+# zstyle ':notify:*' success-title "(⌐■_■)"
 
-zplug "sharat87/zsh-vim-mode"
+zinit load "johnhamelink/env-zsh"
 
-zplug "hchbaw/zce.zsh"
-bindkey "^F" zce
+ZSH_COMMAND_TIME_MIN_SECONDS=15
+ZSH_COMMAND_TIME_MSG="Execution time: %s sec"
+ZSH_COMMAND_TIME_EXCLUDE=(time kak tmx tmux docker-compose)
+zinit load "popstas/zsh-command-time"
 
-zplug "jreese/zsh-titles", if:"which tmux"
-
-zplug "MichaelAquilina/zsh-auto-notify", if:"{ command -v notify-send ; } >/dev/null 2>&1"
-export AUTO_NOTIFY_THRESHOLD=20
-export AUTO_NOTIFY_TITLE="· %command → %exit_code ·"
-export AUTO_NOTIFY_BODY="Duration: %elapsed seconds"
-
-zplug "Tarrasch/zsh-bd"
-
-zplug "Seinh/git-prune", if:"which git"
-
-zplug "johnhamelink/env-zsh"
-
-
-if ! zplug check; then
-  zplug install
-fi
-
-zplug load
+zinit compinit
 
 # configure some plugins after loading
 export ZSH_AUTOSUGGEST_STRATEGY=("match_prev_cmd")
