ef400080a7f274895965f21be4a77a0f92a16302
Sam <sam@wekk.io>
2021-10-04 09:15:06 +0200

    Shell, tools: remove (Neo)Vim tools

--- config/profile/tools.sh
+++ config/profile/tools.sh
@@ -28,35 +28,8 @@ export GIT_EDITOR="${EDITOR}"
 test "${EDITOR}" = "nvim" && alias vim='nvim'
 test "${EDITOR}" != "kak" && DIFFPROG="${EDITOR} -d"
 
-# a simple Vim pager
-vless() {
-  local pager_command=''
-
-  if command -v nvim >/dev/null 2>&1; then
-    pager_command="nvim"
-  else
-    pager_command="vim"
-  fi
-
-  pager_command="${pager_command} -R -c 'nnoremap q :q!<Enter>' -c 'set noswapfile'"
-
-  if [[ "$#" -ne 1 ]]; then
-    pager_command="${pager_command} -"
-  else
-    pager_command="${pager_command} ${1}"
-  fi
-
-  eval "${pager_command}"
-}
-
 # a more compatible difftool
 _default_difftool() {
-  local nvim_path="$(command -v nvim)"
-  [ -n "${nvim_path}" ] && echo "${nvim_path} -d" && return 0
-
-  local vim_path="$(command -v vim)"
-  [ -n "${vim_path}" ] && echo "${vim_path} -d" && return 0
-
   local diff_so_fancy_path="$(command -v diff-so-fancy)"
   [ -n "${diff_so_fancy_path}" ] && echo "${diff_so_fancy_path}" && return 0
 
