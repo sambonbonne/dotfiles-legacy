8a1947ed8ead1e614c84b96c2af6dce9987b2027
Sam <sam@wekk.io>
2021-10-19 11:18:09 +0200

    Kakoune: plugin manager auto-installation

--- install/softwares/kak.sh
+++ install/softwares/kak.sh
@@ -2,8 +2,8 @@
 
 ensure_directory .config/kak
 link_config "config/kak/kakrc"
+link_config "config/kak/colors"
 link_config "config/kak-lsp"
 check_command "kak"
 
-# plugins installation
-git clone https://github.com/robertmeta/plug.kak.git ~/.config/kak/plugins/plug.kak
+# plugins are installed inside Kakoune
