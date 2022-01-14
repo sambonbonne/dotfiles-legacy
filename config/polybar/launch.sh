40fff402060d7c5a17d34fd06cf488f2528a4dfe
Sam <sam@wekk.io>
2022-01-14 14:22:31 +0100

    Remove old unused softwares configs

--- config/polybar/launch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-killall -q polybar
-while pgrep -u "$(id -u)" -x polybar >/dev/null; do sleep 1; done
-
-MONITOR="$(xrandr --query | grep primary | cut -d ' ' -f 1)" polybar top &
